64b86f4b9fe3272b19a17efeefac6897066d8c20
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-10-13 20:18:29 +0600

    Initializing!

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from engine import Sel
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 
 LINK = "https://www.instagram.com/"
-obj = Sel(LINK)
+id = os.environ["ID"]
+password = os.environ["PASS"]
 
+chrome_drive_path = "C:\Development\chromedriver.exe"
+s = Service(chrome_drive_path)
+driver = webdriver.Chrome(service=s)
+driver.get()
 
+item = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')
+item.send_keys("00")
 
-
-# obj.Quit()
+# driver.quit()
