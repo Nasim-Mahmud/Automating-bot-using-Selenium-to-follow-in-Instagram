15b2ef9248699934ca58b9bfb9f530194ae3dd13
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-10-14 11:38:56 +0600

    Logging into account!

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 
 LINK = "https://www.instagram.com/"
@@ -24,6 +25,16 @@ sleep(1)
 passw = driver.find_element(By.NAME, "password")
 passw.send_keys(password)
 sleep(1)
+
+submit = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button/div')
+submit.click()
+sleep(10)
+
+search = driver.find_element(By.XPATH, '//input[@placeholder="Search"]')
+search.send_keys("cookingchannel")
+search.send_keys(Keys.ENTER)
+sleep(4)
+
 
 
 # driver.quit()
